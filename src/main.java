0480762cdfafe692c9f59173cdb4d83ab46607cb
Guidin08 <pisarraguido@gmail.com>
2021-06-07 20:42:12 -0300

    se agrega el criterio SaleConFritas

--- src/main.java
+++ src/main.java
@@ -1,40 +1,30 @@
 import criterioComida.*;
 import app.*;
+import criterioCosto.ImporteCondicional;
 
 
 public class main {
     public static void main (String args[]){
         Cocina palacio =new Cocina();
 
-        ComidaCompuesta macarons = new ComidaCompuesta("macarons","postre","mucho trabajo");
+        ComidaCompuesta macarons = new ComidaCompuesta("macarons","postre","horno");
         ComidaSimple ganache =new ComidaSimple("ganache","postre","se compra",110,1200,30);
-        ComidaSimple tapaMacarons =new ComidaSimple("tapamacarons","postre","lleva su tiempo",130,1780,45);
+        ComidaSimple tapaMacarons =new ComidaSimple("tapamacarons","postre","horno",130,1780,45);
         macarons.addComidaSimple(ganache);
         macarons.addComidaSimple(tapaMacarons);
-
-        /*Pedido mesa1= new Pedido(1,"pepe");
-        TipoComida esPostre= new TipoComida("postre");
-        palacio.agregarCriterioAdicionalComida(esPostre,10);
-        mesa1.agregarAlPedido(macarons);
-        mesa1.agregarAlPedido(macarons);
+        Pedido mesa1=new Pedido(8,"Pepe");
         mesa1.agregarAlPedido(macarons);
 
-        System.out.println(("Mesa 1 "+ palacio.getCostoPedido(mesa1)));*/
+        ModoPreparacion horno=new ModoPreparacion("horno");
+        ImporteCondicional diaPlatoFrances=new ImporteCondicional(horno,100,10);
+        Estacion estacion1=new Estacion("estacion1",horno);
+        palacio.addEstacion(estacion1);
+        palacio.asignarPedidoEstacion(mesa1);
+        System.out.println(mesa1.costoTotalDeMesa(diaPlatoFrances));
+        System.out.println(mesa1.costoTotalDeMesa());
 
 
-        //TODO OJO ACA PRUEBO EL ENGENDRO QUE HICE VER CON FEDE
 
-        Pedido mesa2=new Pedido(2,"pepe2");
-        mesa2.agregarAlPedido(macarons);
-        mesa2.agregarAlPedido(macarons);
-        mesa2.agregarAlPedido(macarons);
-        //AdicionalPorPorcentaje porcentaje=new AdicionalPorPorcentaje("cualquiera",100);
-        //palacio.addCriterioCobroAdicional(porcentaje);
-        //Descuento descuento=new Descuento("descontar",10);
-        //palacio.addCriterioCobroAdicional(descuento);
-        //SumarAdicionalExtra sumo=new SumarAdicionalExtra("extra",80);
-        //palacio.addCriterioCobroAdicional(sumo);
-        System.out.println("Mesa 2 "+palacio.cerrarMesa(mesa2));
 
 
 
